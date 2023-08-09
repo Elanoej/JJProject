e681f1bf9b7509e298ba6bf180f5e1f89c455597
Elano Marcena <elanomarcena@gmail.com>
2023-08-09 12:11:29 -0300

    Added Client and ServiceOrder models

---
+++ src/main/java/com/eletronica/JJProject/model/Client.java
@@ -0,0 +1,87 @@
+package com.eletronica.JJProject.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column
+    private String address;
+    @Column(nullable = false)
+    private String cellphone;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private ServiceOrder serviceOrder;
+
+    public Client(){
+    }
+
+    public Client(Integer id, String name, String address, String cellphone){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cellphone = cellphone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public ServiceOrder getServiceOrder() {
+        return serviceOrder;
+    }
+
+    public void setServiceOrder(ServiceOrder serviceOrder) {
+        this.serviceOrder = serviceOrder;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(name, client.name) && Objects.equals(address, client.address) && Objects.equals(cellphone, client.cellphone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, cellphone);
+    }
+}
---
+++ src/main/java/com/eletronica/JJProject/model/ServiceOrder.java
@@ -0,0 +1,105 @@
+package com.eletronica.JJProject.model;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "service-order")
+public class ServiceOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Instant date;
+    @OneToOne
+    @MapsId
+    private Client client;
+    private String productModel;
+    private String productDetails;
+    private String clientInfos;
+    private String tecInfos;
+
+    public ServiceOrder(Integer id, Instant date, Client client, String productModel, String productDetails, String clientInfos, String tecInfos) {
+        this.id = id;
+        this.date = date;
+        this.client = client;
+        this.productModel = productModel;
+        this.productDetails = productDetails;
+        this.clientInfos = clientInfos;
+        this.tecInfos = tecInfos;
+    }
+
+    public ServiceOrder() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getProductModel() {
+        return productModel;
+    }
+
+    public void setProductModel(String productModel) {
+        this.productModel = productModel;
+    }
+
+    public String getProductDetails() {
+        return productDetails;
+    }
+
+    public void setProductDetails(String productDetails) {
+        this.productDetails = productDetails;
+    }
+
+    public String getClientInfos() {
+        return clientInfos;
+    }
+
+    public void setClientInfos(String clientInfos) {
+        this.clientInfos = clientInfos;
+    }
+
+    public String getTecInfos() {
+        return tecInfos;
+    }
+
+    public void setTecInfos(String tecInfos) {
+        this.tecInfos = tecInfos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceOrder that = (ServiceOrder) o;
+        return Objects.equals(id, that.id) && Objects.equals(date, that.date) && Objects.equals(client, that.client) && Objects.equals(productModel, that.productModel) && Objects.equals(productDetails, that.productDetails) && Objects.equals(clientInfos, that.clientInfos) && Objects.equals(tecInfos, that.tecInfos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, client, productModel, productDetails, clientInfos, tecInfos);
+    }
+}
